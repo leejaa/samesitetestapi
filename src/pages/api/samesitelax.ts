a2f624f333016e11ca54f801d62c6ffe99b73505
break-jahun <jh.lee@break.co.kr>
2023-07-23 13:31:49 +0900

    Update samesitelax.ts

--- src/pages/api/samesitelax.ts
+++ src/pages/api/samesitelax.ts
@@ -18,6 +18,6 @@ export default async function handler(
     //     sameSite: 'lax'
     // })
     
-    res.setHeader('Set-Cookie', 'token=asdfasdf; SameSite=Lax')
+    res.setHeader('Set-Cookie', 'token=asdfasdf; SameSite=Lax; Secure')
   res.status(200).json({ name: 'John Doe' })
 }
